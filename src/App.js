4a1fb46982041c9ce2dc8b2e5d14d9b9f736c750
sytnikov <sytnikov.alexey@gmail.com>
2023-06-13 18:45:52 +0300

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Meet the dentist',
+    },
+    {
+      id: 2,
+      text: 'Clean the room',
+    },
+    {
+      id: 3,
+      text: 'Do the shopping',
+    }
+  ])
+  
   return (
     // you can have only one parent component (in this case it's div)
     <div className="container">
-        <Header title="Task Tracker"/>
-        <Tasks />
+      <Header title="Task Tracker"/>
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,19 +1,4 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Meet the dentist',
-    },
-    {
-        id: 2,
-        text: 'Clean the room',
-    },
-    {
-        id: 3,
-        text: 'Do the shopping',
-    }
-]
-
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => <h3 key={task.id}>{task.text}</h3>)}
