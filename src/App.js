f7960587e745573db61144847b58542bc7ea60f6
sytnikov <sytnikov.alexey@gmail.com>
2023-06-16 16:12:34 +0300

    added task from in the container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -8,16 +9,19 @@ function App() {
       id: 1,
       text: 'Meet the dentist',
       day: 'Feb 5th at 2:30pm',
+      reminder: true,
     },
     {
       id: 2,
       text: 'Clean the room',
       day: 'Feb 6th at 6:30pm',
+      reminder: true,
     },
     {
       id: 3,
       text: 'Do the shopping',
       day: 'Feb 8th at 8:00pm',
+      reminder: false
     }
   ])
 
@@ -26,11 +30,17 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   }
 
+  // Toggle Reminder function
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task));
+  }
+
   return (
     // you can have only one parent component (in this case it's div)
     <div className="container">
-      <Header title="Task Tracker"/>
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      <Header title="Task Tracker" />
+      <AddTask />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks to show'}
     </div>
   );
 }
