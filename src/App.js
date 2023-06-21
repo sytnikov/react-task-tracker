7773243587d40ee56e056130557bb8db08866e5a
sytnikov <sytnikov.alexey@gmail.com>
2023-06-21 17:45:50 +0300

    toggle reminder on server

--- src/App.js
+++ src/App.js
@@ -23,6 +23,13 @@ const App = () => {
     return data;
   }
 
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  }
+
   // Add Task function
   const addTask = async (task) => {
     const res = await fetch('http://localhost:5000/tasks', {
@@ -52,8 +59,25 @@ const App = () => {
   }
 
   // Toggle Reminder function
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task));
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    });
+
+    const data = await res.json();
+
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? {...task, reminder: data.reminder} : task
+      )
+    );
   }
 
   return (
